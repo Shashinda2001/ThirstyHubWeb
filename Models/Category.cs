61fe00946bc17f0117dea5821b782f26ff6cdfbd
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-21 10:02:27 +0530

    create Category model with one-to-many relationship to Drink model

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace ThirstyHubWeb.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        public List<Drink> Drinks { get; set; }
+        
+    }
+}

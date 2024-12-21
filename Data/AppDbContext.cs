093704461af605563f73c3005e582c010ba591e4
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-21 16:42:32 +0530

    Fix syntax error in appsettings.json: Added missing comma after AllowedHosts property

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThirstyHubWeb.Data.Models;
+
+namespace ThirstyHubWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Drink> Drinks { get; set; }
+        public DbSet<Category> Categories { get; set; } 
+
+    }
+}

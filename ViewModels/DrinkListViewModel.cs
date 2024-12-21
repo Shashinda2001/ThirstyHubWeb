57522ba761ad8831990d3282d8614842daf14778
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-21 12:25:01 +0530

    Added DrinkListViewModel to hold list of drinks and current category

---
+++ ViewModels/DrinkListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ThirstyHubWeb.Data.Models;
+
+namespace ThirstyHubWeb.ViewModels
+{
+    public class DrinkListViewModel
+    {
+        public IEnumerable<Drink> Drinks { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}

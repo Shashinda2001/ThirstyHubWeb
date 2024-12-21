d28c8b42e2ea609d42d9e91900907f297a32dc20
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-21 11:45:09 +0530

    create  DrinkController

---
+++ Controllers/DrinkController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThirstyHubWeb.Data.Interfaces;
+
+namespace ThirstyHubWeb.Controllers
+{
+    public class DrinkController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IDrinkRepository _drinkRepository;
+
+        public DrinkController(ICategoryRepository categoryRepository, IDrinkRepository drinkRepository )
+        {
+            _categoryRepository = categoryRepository;
+            _drinkRepository = drinkRepository;
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ViewResult List()
+        {
+            var drinks = _drinkRepository.Drinks;
+            return View( drinks );
+        }
+    }
+}

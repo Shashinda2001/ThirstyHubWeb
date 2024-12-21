2eb59fe37c4db022c5f94f34d4d08455d183ab59
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-12-21 10:13:09 +0530

    Create interface IDrinkRepository

---
+++ Interfaces/IDrinkRepository.cs
@@ -0,0 +1,14 @@
+﻿using ThirstyHubWeb.Models;
+
+namespace ThirstyHubWeb.Interfaces
+{
+    public interface IDrinkRepository
+    {
+        IEnumerable<Drink> Drinks { get; set; }
+        IEnumerable<Drink> PerferreOrders { get; set; }
+
+        Drink GetDrinkById(int drinkId);
+
+
+    }
+}
